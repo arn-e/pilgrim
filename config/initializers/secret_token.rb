eb54837869573e50963ad292466ebb828ba9baaa
Arne Fostvedt <arne@Arnes-MacBook-Air.local>
2013-02-24 17:57:33 -0800

    initialized repo, created README

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pilgrim::Application.config.secret_token = 'db631d88b7005df4f42484499bccef16979f3456b90d833d0669ce805feb0d110de23766de5692f3d00b53277947f12ec16d2ac91aeaa7a24f088e461ee18159'
