7a1b24ac16d9f37ecd2c38ffcaf19e787c59e6f1
eso <eso@methods.lan>
2013-04-30 22:34:19 -0400

    updating project, removing stuff

--- node/app.js
+++ node/app.js
@@ -1,16 +1,6 @@
 var express = require('express'),
-    mongo_parser = require('./src/mongo_parser'),
-    mongohq_connection = require('./src/mongohq_connection'),
     app = express();
 
-// configuring express
-app.use(express.bodyParser());
-app.use(express.static(__dirname + '../public'));
-app.engine('.html', require('jade').__express);
-app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
-
-// default route
 app.get('/', function(req, res) {
   res.render('index.jade');
 });
