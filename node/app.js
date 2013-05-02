4b9ec904789d8b3bec04f470dc071a3917509598
eso <eso@methods.lan>
2013-05-01 22:45:56 -0400

    udpated with some notesy notes

--- node/app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express'),
-    app = express();
-
-app.get('/', function(req, res) {
-  res.render('index.jade');
-});
-
-app.listen(3000);
-console.log('Listening on port 3000...');
